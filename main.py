8977a3e27f4cf9f65cca578bd70b6bd928e073b9
ldinghan <86869693+ldinghan@users.noreply.github.com>
2022-04-26 09:49:46 +0800

    Automatically opens up the excel file after running

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from ntuc_scraper import scrape as NTUCscrape
 from giant_scraper import scrape as GIANTscrape
 from displaydata import getSheet
+import os
+
+def clearConsole():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
 
 def main():
 	chromedriver_path = "C:/Users/user/.wdm/drivers/chromedriver/win32/99.0.4844.51/chromedriver.exe"
@@ -15,8 +22,9 @@ def main():
 			print(query)
 			getSheet(query, NTUCscrape(query, chromedriver_path), GIANTscrape(query, chromedriver_path))
 			count += 1
+			clearConsole()
 			print(f'{count} item(s) out of {total} completed')
-
+	os.system('"xlwt result.xls"')
 
 
 if __name__ == "__main__":
